15141a86f0b7d32bcd8b20bf187cda9973874147
beardnick <1685437606@qq.com>
2019-04-03 00:27:14 +0800

    添加测试笔记

--- shell.c
+++ shell.c
@@ -355,6 +355,12 @@ _cygwin32_check_tmp ()
 
 #if defined (NO_MAIN_ENV_ARG)
 /* systems without third argument to main() */
+/**
+ * NOTE: 2019-04-03:00:19
+ * main函数声明的一种写法
+ * 等效于
+ * int main(int argc, char **argv)
+ */
 int
 main (argc, argv)
      int argc;
@@ -465,7 +471,7 @@ main (argc, argv, env)
 
   /* Find full word arguments first. */
   arg_index = parse_long_options (argv, arg_index, argc);
-  
+
   if (want_initial_help)
     {
       show_shell_usage (stdout, 1);
@@ -1562,7 +1568,7 @@ open_shell_script (script_name)
 #if defined (EISDIR)
 	      errno = EISDIR;
 	      file_error (filename);
-#else	      
+#else
 	      internal_error (_("%s: Is a directory"), filename);
 #endif
 	    }
@@ -1678,7 +1684,7 @@ unset_bash_input (check_zero)
     }
 #endif /* !BUFFERED_INPUT */
 }
-      
+
 
 #if !defined (PROGRAM)
 #  define PROGRAM "bash"
